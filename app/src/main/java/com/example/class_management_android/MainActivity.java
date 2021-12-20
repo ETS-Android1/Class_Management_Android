aa8331057ae7858493296d43229c0dbf8992f012
tungbk62 <phantungbk62@gmail.com>
2021-12-20 16:49:14 +0700

    Migrate to AndroidX

--- app/src/main/java/com/example/class_management_android/MainActivity.java
+++ app/src/main/java/com/example/class_management_android/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.class_management_android;
 
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
--- app/src/main/java/com/example/class_management_android/StudentsListActivity.java
+++ app/src/main/java/com/example/class_management_android/StudentsListActivity.java
@@ -1,14 +1,14 @@
 package com.example.class_management_android;
 
 import android.app.AlertDialog;
-import android.arch.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleObserver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
